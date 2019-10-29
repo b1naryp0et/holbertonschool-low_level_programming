396e22a4d3f5a33f10148c0d5904c2d4e10d3894
Faizan Khan <mathemagician229@gmail.com>
2019-10-29 01:43:00 +0000

    Adding first task to test.

---
+++ 0x1B-sorting_algorithms/0-bubble_sort.c
@@ -0,0 +1,34 @@
+#include "sort.h"
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * bubble_sort - Sort
+ * @array: array
+ * @size: size
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, x;
+	int tmpvrbl;
+
+	if (size == 0 || array == NULL)
+		return;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		for (x = 0; x < size - 1 - i; x++)
+		{
+			if (array[x] > array[x + 1])
+			{
+				tmpvrbl = array[x + 1];
+				array[x + 1] = array[x];
+				array[x] = tmpvrbl;
+				print_array(array, size);
+			}
+		}
+		x = 0;
+	}
+}
