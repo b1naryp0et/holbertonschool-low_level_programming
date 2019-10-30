555f135a2cfd66170f02d07e6eca4d181a6fd54d
Faizan Khan <mathemagician229@gmail.com>
2019-10-30 23:39:41 +0000

    Thanks Mitch and Carter.

--- 0x1B-sorting_algorithms/1-insertion_sort_list.c
+++ 0x1B-sorting_algorithms/1-insertion_sort_list.c
@@ -1,11 +1,10 @@
 #include "sort.h"
-#include <stdio.h>
-#include <stdlib.h>
 
 /**
  * insertion_sort_list - Sort
  * @list: List
  */
+
 void insertion_sort_list(listint_t **list)
 {
 	listint_t *x;
@@ -13,43 +12,32 @@ void insertion_sort_list(listint_t **list)
 	listint_t *z;
 
 	if (!list || !(*list))
-
 		return;
-
 	for (y = x = (*list)->next; y != NULL; y = x = y->next)
-
 	{
 		z = x->prev;
-
 		while (z != NULL && x->n < z->n)
 			z = z->prev;
-
 		if (z == x->prev)
 			continue;
-
 		y = x->prev;
 		if (x->next != NULL)
 			x->next->prev = x->prev;
-
 		x->prev->next = x->next;
 		if (z)
-
 		{
 			x->next = z->next;
 			x->prev = z;
 			z->next = x;
 			x->next->prev = x;
 		}
-
 		else
-
 		{
 			x->prev = NULL;
 			x->next = *list;
 			(*list)->prev = x;
 			*list = x;
 		}
-
 		print_list(*list);
 	}
 }
