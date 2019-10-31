4a6e2036ea241abd387f99465d1f5f18ef764109
Faizan Khan <mathemagician229@gmail.com>
2019-10-31 02:46:44 +0000

    Adding task 3.

--- 0x1B-sorting_algorithms/sort.h
+++ 0x1B-sorting_algorithms/sort.h
@@ -32,5 +32,6 @@ void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
+void quick_sort(int *array, size_t size);
 
 #endif
