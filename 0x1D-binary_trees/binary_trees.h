d68738cc6781ac105e67a035fc3bc2e77731bce7
Faizan Khan <mathemagician229@gmail.com>
2019-12-06 20:21:33 +0000

    Adding first advanced task.

--- 0x1D-binary_trees/binary_trees.h
+++ 0x1D-binary_trees/binary_trees.h
@@ -48,4 +48,6 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 /* Advanced */
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
 int binary_tree_is_bst(const binary_tree_t *tree);
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+				     const binary_tree_t *second);
 #endif
