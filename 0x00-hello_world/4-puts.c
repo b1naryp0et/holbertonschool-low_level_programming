e21dbf66400af400f05af0c6b7629fd58e6e28b2
Faizan Khan <mathemagician229@gmail.com>
2020-01-01 01:46:08 +0000

    Adding completed mandatory tasks.

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Prints a string
+ *
+ * Return: 0 for exit
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
